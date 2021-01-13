ea81ae96b47412c26cbd00c44882cf9d5779cb7b
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-13 18:34:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,17 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/)
+    true
+  else
+    false
+  end
 end
