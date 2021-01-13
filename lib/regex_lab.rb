5ffe742ada5d909140df40a12773a88d0254f9cc
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-13 18:26:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,8 @@
 def starts_with_a_vowel?(word)
-  if word.match(/[aeiou]+\w/i) 
+  if word.match(/[aeiou]+\w/i)
     return true
-  else
-    return false
   end
+  return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
